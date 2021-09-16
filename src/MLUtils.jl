e3f8f204cae90a2a7601f1932653616b7f100d5a
NFSturm <niclas.sturm@live.de>
2021-09-16 16:19:56 +0100

    Added F1-Score and fixed dependencies

--- src/MLUtils.jl
+++ src/MLUtils.jl
@@ -3,8 +3,9 @@ module MLUtils
 using StatsBase
 using UnPack
 using MLStyle.Modules.Cond
+using DataFrames
 
-export confusion_matrix, precision, recall
+export confusion_matrix, precision, recall, f1_score, make_train_test_data
 
 round_prediction(pred::Vector{Float64}, threshold::Float64) = pred > threshold ? 1 : 0
 
@@ -22,6 +23,7 @@ function eval_obs(pred_obs::Int64, true_obs::Int64)::String
 		pred_obs == 0 && true_obs == 0 => "TN"
 	end
 end
+
 
 @doc """
     confusion_matrix(predictions::Vector{Float64}, true_labels::Vector{Int64}, true_threshold::Float64)::Matrix{Int64}
@@ -43,12 +45,55 @@ function confusion_matrix(predictions::Vector{Float64}, true_labels::Vector{Int6
 
 end
 
+
+@doc """
+Calculates the precision of a model based on the confusion matrix.
+"""
 function precision(confmat::Matrix{Int64})
-	confmat[1, 1] / ( confmat[1, 1] + confmat[2, 1])
+	confmat[1, 1] / ( confmat[1, 1] + confmat[2, 1] )
 end
 
+
+@doc """
+Calculates the recall of a model based on the confusion matrix.
+"""
 function recall(confmat::Matrix{Int64})
-	confmat[1, 1] / ( confmat[1, 1] + confmat[1, 2]
+	confmat[1, 1] / ( confmat[1, 1] + confmat[1, 2] )
+end
+
+
+@doc """
+Calculates the F1 score based on the confusion matrix.
+"""
+function f1_score(confmat::Matrix{Int64})
+	2 * ( precision(confmat) * recall(confmat) ) / ( precision(confmat) + recall(confmat) )
+end
+
+
+@doc """
+    make_train_test_data(all_data::DataFrame, label_column::String, feature_column::String, train_prop::Float64)
+
+Randomly shuffles `all_data` according to `train_prop`. Returns the training and test data including the corresponding labels.
+"""
+function make_train_test_data(all_data::DataFrame, feature_column::String, label_column::String, train_prop::Float64)
+
+	all_labels = all_data[!, label_column]
+	all_features = all_data[!, feature_column]
+
+	data_length = nrow(all_data)
+
+	indices = shuffle(1:1:data_length)
+
+	split_index = Int(floor(data_length * train_prop))
+
+	train_indices = indices[1:split_index]
+	test_indices = indices[split_index:end]
+
+	X_train, y_train = convert(Array{Float64}, transpose(hcat(all_features[train_indices]...))), convert(Array{Int64}, all_labels[train_indices])
+	X_test, y_test = convert(Array{Float64}, transpose(hcat(all_features[test_indices]...))), convert(Array{Int64}, all_labels[test_indices])
+
+	return X_train, y_train, X_test, y_test
+
 end
 
 end
