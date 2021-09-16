4539616e9ad8c29b08d60ac8b84a8758cb1fdb23
NFSturm <niclas.sturm@live.de>
2021-09-16 16:20:18 +0100

    Changed ordering of algorithm. Started to implement grid embedding.

--- src/SymmachusModel.jl
+++ src/SymmachusModel.jl
@@ -1,12 +1,14 @@
 # Self-Training
 
 using XGBoost
-using DataFrames
 using Parameters
 using Serialization
 using Chain
 using Random
 using Distributed
+using CSV
+using DataFrames
+using StatsBase
 
 addprocs(5)
 
@@ -14,16 +16,21 @@ addprocs(5)
 	using Pkg; Pkg.activate(".")
 
 	using XGBoost
-	using DataFrames
 	using Parameters
 	using Serialization
 	using Chain
 	using Random
 	using Distributed
+	using CSV
+	using DataFrames
+	using StatsBase
 end
 
 @everywhere include("SymmachusCore.jl")
+@everywhere include("MLUtils.jl")
+
 @everywhere using .SymmachusCore
+@everywhere using .MLUtils
 
 @everywhere begin
 	embeddings_vocab, embeddings = load_fasttext_embeddings("./data/embeddings")
@@ -32,17 +39,47 @@ end
 
 	global embeddings_lookup = Lookup(word_lookup_table, embeddings, embeddings_vocab)
 
-	@with_kw mutable struct ModelArgs
+	@with_kw mutable struct SymmachusArgs
 		max_discourse_context_size::Int64
 		max_sentence_context_size::Int64
 		self_weight::Number
 	end
 end
 
-@everywhere global symmachus_args = ModelArgs(
+@everywhere symmachus_args = SymmachusArgs(
 	max_discourse_context_size=3,
 	max_sentence_context_size=3,
 	self_weight=0.8
+)
+
+@everywhere grid = Dict(:max_discourse_context_size => 1:5, :max_sentence_context_size => 1:5, :self_weight => 0.5:0.1:0.9, :grid_size => 10)
+
+@everywhere symmachus_args = [SymmachusArgs(
+	max_discourse_context_size = sample(grid[:max_discourse_context_size], 1) |> first,
+	max_sentence_context_size = sample(grid[:max_sentence_context_size], 1) |> first,
+	self_weight = sample(grid[:self_weight], 1) |> first
+) for i in 1:grid[:grid_size]]
+
+
+@with_kw mutable struct BoostingArgs
+	num_rounds::Int64 # Number of rounds for training the booster
+	metrics::Vector{String} # The metric to be chosen
+	params::Vector{Pair{String, Any}} # Model parameters
+	true_threshold::Float64 # Threshold for positive prediction
+	train_prop::Float64 # Proportion of observations to be used for training
+end
+
+
+boosting_args = BoostingArgs(
+	num_rounds=150,
+	metrics=["aucpr"],
+	params= [
+		"max_depth" => 2,
+		"eta" => 1,
+		"objective" => "binary:logistic"
+	],
+	true_threshold=0.5,
+	train_prop=0.8
 )
 
 @everywhere @doc """
@@ -57,12 +94,12 @@ end
 
 
 @everywhere @doc """
-    doc_to_sent(file::String)
+    doc_to_sent(file::String, symmachus_args:SymmachusArgs)
 
 Using the `embeddings_lookup`, document files are read from the directory and \n
 and embedded. The output is a dataframe containing the individual sentences. \n
 """
-function doc_to_sent(file::String)
+function doc_to_sent(file::String, symmachus_args::SymmachusArgs)
 
     embedded_dataframe = DataFrame(
         doc_uuid = String[],
@@ -77,9 +114,9 @@ function doc_to_sent(file::String)
 
     # Unpacking the arguments
 
-    @unpack max_discourse_context, max_sentence_context, self_weight = args
+    @unpack max_discourse_context_size, max_sentence_context_size, self_weight = symmachus_args
 
-    emb_doc = embed_document(doc, max_discourse_context, max_sentence_context, self_weight, embeddings_lookup)
+    emb_doc = embed_document(doc, max_discourse_context_size, max_sentence_context_size, self_weight, embeddings_lookup)
 
     sentences = doc.sentences
     dataframe_rows = make_dataframe_row.(sentences, emb_doc)
@@ -101,6 +138,7 @@ labelled_sentences = label_data[!, [:doc_uuid, :sentence_id]]
 
 make_path_to_speech_docs(doc_name::String) = "./data/speech_docs/" * doc_name * ".jls"
 
+
 @doc """
     make_deserialization_paths(items::Vector{String})
 
@@ -121,18 +159,15 @@ end
 
 deserialization_paths = make_deserialization_paths(deserialization_items)
 
-
 @doc """
-    make_document_dataframe(paths::Vector{String}, model_args)
+    make_document_dataframe(paths::Vector{String}, symmachus_args::SymmachusArgs)
 
 Deserializes documents and embeds the documents contained in `paths`. \n
-These documents are then split into sentences and appended to a dataframe. \n
-Parameters of the *SymmachusModel* can be specified by `model_args`.
+These documents are then split into sentences and appended to a dataframe.
 """
-function make_document_dataframe(paths::Vector{String})
-    res = pmap(doc_to_sent, paths)
+function make_document_dataframe(paths::Vector{String}, symmachus_args::SymmachusArgs)
+	res = pmap((paths, symmachus_args) -> doc_to_sent(paths, symmachus_args), paths, symmachus_args)
 end
-
 
 @doc """
     concat_dataframes(dataframes::Vector{DataFrame})::DataFrame
@@ -143,93 +178,68 @@ function concat_dataframes(dataframes::Vector{DataFrame})::DataFrame
     vcat(dataframes..., cols=:union)
 end
 
-document_dataframe = make_document_dataframe(deserialization_paths, model_args) |> concat_dataframes
+document_dataframe = [make_document_dataframe(deserialization_paths, symmachus_arg) |> concat_dataframes for symmachus_arg in symmachus_args]
 
-sentence_label_data  = innerjoin(label_data, document_dataframe, on=[:doc_uuid, :sentence_id], makeunique=true)
-
-@doc """
-    make_train_test_data(all_data::DataFrame, label_column::String, feature_column::String, train_prop::Float64)
-
-Randomly shuffles `all_data` according to `train_prop`. Returns the training and test data including the corresponding labels.
-"""
-function make_train_test_data(all_data::DataFrame, feature_column::String, label_column::String, train_prop::Float64)
-
-	all_labels = all_data[!, label_column]
-	all_features = all_data[!, feature_column]
-
-	data_length = nrow(all_data)
-
-	indices = shuffle(1:1:data_length)
-
-	split_index = Int(floor(data_length * train_prop))
-
-	train_indices = indices[1:split_index]
-	test_indices = indices[split_index:end]
-
-	X_train, y_train = all_features[train_indices], all_labels[train_indices]
-	X_test, y_test = all_features[test_indices], all_labels[test_indices]
-
-	return X_train, y_train, X_test, y_test
-
-end
-
-X_train, y_train, X_test, y_test = make_train_test_data(sentence_label_data, "sentence_embedding", "label", 0.8)
-
-@with_kw mutable struct BoostingArgs
-	num_rounds::Int64
-	nfold::Int64
-	metrics::String
-	params::Vector{Pair{String, Any}}
-end
-
-
-boosting_args = BoostingArgs(
-	num_rounds=50,
-	nfold=2,
-	metrics="precision",
-	params= [
-		"max_depth" => 2,
-		"eta" => 1,
-		"objective" => "binary:logistic"
-	]
-)
+sentence_label_data  = innerjoin(labelled_sentences, document_dataframe, on=[:doc_uuid, :sentence_id], makeunique=true)
 
 
 @doc """
-    train_booster(feature_data::Vector{Vector{Float64}}, label_data::Vector{Int64}}, boosting_args)
-
-Trains a boosting classifier.
-"""
-function train_booster(feature_data::Vector{Vector{Float64}}, label_data::Vector{Int64}}, boosting_args)
-
-	nfold_cv(feature_data, boosting_args.num_rounds, boosting_args.nfold, label = label_data, param = param, metrics = boosting_args.metrics)
-
-end
-
-
-@doc """
-    sample_documents(all_documents_path::String, labelled_documents::Vector{String})::Vector{String}
+    sample_documents(all_documents_path::String, labelled_documents::Vector{String}, num_documents::Int64)::Vector{String}
 
 Samples documents from a directory. Returns a vector of strings.
 """
-function sample_documents(all_documents_path::String, labelled_sentences::Vector{String})::Vector{String}
+function sample_documents(all_documents_path::String, labelled_sentences::Vector{String}, num_documents::Int64)::Vector{String}
 	all_documents = readdir(all_documents_path)
 	all_documents_id = first.(split.(all_documents, Ref('.')))
 
 	uuid_labels = labelled_sentences[!, :doc_uuid] |> Set |> collect
 
-	# Do an anti-join with existing labels after deserialization
+	labels = labelled_sentences[!, :doc_uuid]
 
+	existing_labels = labelled_sentences[!, :doc_uuid] |> Set |> collect
+
+	document_samples = sample(all_documents_id, 200)
+
+	# Sample only those documents that have not yet been labelled.
+	zips = zip(document_samples, document_samples .∈ Ref(existing_labels)) |> collect
+
+	[zip[1] for zip in zips if !zip[2]]
 end
 
 
+@doc """
+    train_booster(feature_data::Vector{Vector{Float64}}, label_data::Vector{Int64}}, boosting_args::BoostingArgs)
 
-all_documents = readdir("./data/speech_docs")
+Trains a boosting classifier.
+"""
+function train_booster(feature_data::Matrix{Float64}, label_data::Vector{Int64}, boosting_args::BoostingArgs)
+	xgboost(feature_data, boosting_args.num_rounds, label=label_data, param = boosting_args.params, metrics = boosting_args.metrics)
+end
 
-all_documents_id = first.(split.(documents, Ref('.')))
 
-labels = labelled_sentences[!, :doc_uuid]
+@doc """
+    predict_booster(booster::Booster, test_data)
 
-labelled_sentences[!, :doc_uuid] |> Set |> collect
+Predict using a `booster` object.
+"""
+function predict_booster(booster::Booster, test_data)
+	convert(Vector{Float64}, predict(booster, test_data))
+end
 
-docu
+
+@doc """
+    boost(feature_data::Matrix{Float64}, label_data::Vector{Int64}, boosting_args::BoostingArgs)
+
+Trains a booster on `feature_data` and `label_data`. Arguments of the model can be \n
+specified using `boosting_args`.
+"""
+function boost(feature_data::Matrix{Float64}, label_data::Vector{Int64}, boosting_args::BoostingArgs)
+	X_train, y_train, X_test, y_test = make_train_test_data(sentence_label_data, "sentence_embedding", "label", boosting_args.train_prop)
+	bst = train_booster(X_train, y_train, boosting_args)
+	predictions = predict_booster(bst, X_test)
+	confmat = confusion_matrix(predictions, y_test, 0.5)
+	f1_model_score = f1_score(confmat)
+
+	return Dict(:f1_score => f1_model_score, :model_args => boosting_args)
+
+end
