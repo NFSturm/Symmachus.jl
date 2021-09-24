46fdc4cba5632dc0df73e54449ff0d282faf65f5
NFSturm <niclas.sturm@live.de>
2021-09-24 19:21:00 +0100

    Simplified regex rules

--- src/Lemmatization.jl
+++ src/Lemmatization.jl
@@ -11,14 +11,9 @@ Lemmatizes Portuguese plural nouns, based on rules defined in the body.
 """
 function lemmatize(text::String)::String
     @chain text begin
-        replace(_, r"os" => s"o")
-        replace(_, r"as" => s"a")
-        replace(_, r"ais" => s"al")
-        replace(_, r"eis" => s"el")
-        replace(_, r"ois" => s"ol")
-        replace(_, r"ãos" => s"ão")
-        replace(_, r"ões" => s"ão")
-        replace(_, r"ães" => s"ão")
+        replace(_, r"([ao])s" => s"\1")
+        replace(_, r"([eao])is" => s"\1l")
+        replace(_, r"[ãõ][eo]s" => s"ão")
         replace(_, r"([rzns])es\s*" => s"\1")
     end
 end
