3a08e1a52170b9560ed0fd1a43fb427708e74033
NFSturm <niclas.sturm@live.de>
2021-10-14 12:36:25 +0100

    Got setup script working

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo dnf install julia
 git clone https://github.com/NFSturm/Symmachus.jl.git
 cd Symmachus.jl
-
-
+julia setup_script.jl # Instantiating packages
 
 # Running the model
+julia src/SymmachusModel.jl
