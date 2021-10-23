c1c1472d5338acf52473215e906fc7a02a9bd7d8
NFSturm <niclas.sturm@live.de>
2021-10-23 14:12:27 +0100

    Update project depts & adding plotting script for reproduction

---
+++ analyses/TrainingAnalysis.jl
@@ -0,0 +1,57 @@
+using DataFrames
+using StatsBase
+using Gadfly
+using Serialization
+using Pipe
+
+#********************** DEFINING STRUCTS FOR DESERIALIZATION ******************
+
+mutable struct SymmachusArgs
+    max_discourse_context_size::Int64
+    max_sentence_context_size::Int64
+    self_weight::Number
+end
+
+mutable struct BoostingArgs
+    num_rounds::Int64 # Number of rounds for training the booster
+    metrics::Vector{String} # The metric to be chosen
+    params::Vector{Pair{String, Any}} # Model parameters
+    true_threshold::Float64 # Threshold for positive prediction
+    train_prop::Float64 # Proportion of observations to be used for training
+end
+
+#********************** DESERIALIZING MODEL HISTORY ****************************
+
+model_history = deserialize("./cache/final_model/model_history.jls")
+
+@doc """
+    create_performance_dataframe(model_history::Vector{Dict})::DataFrame
+
+Creates a DataFrame with model performance from `model_history`.
+"""
+function create_performance_dataframe(model_history::Vector{Dict})::DataFrame
+
+    performance_dataframe = DataFrame(
+                    epoch = Int64[],
+                    f1_score = Float64[]
+                    )
+
+    performance_history = [(i, epoch[:performance]) for (i,epoch) in enumerate(model_history)]
+
+    for row in performance_history
+        push!(performance_dataframe, row)
+    end
+
+    performance_dataframe
+end
+
+performance_dataframe = create_performance_dataframe(model_history)
+
+plot(performance_dataframe,
+    x=:epoch,
+    y=:f1_score,
+    Geom.point,
+    Geom.line,
+    Guide.xlabel("Epoch No."),
+    Guide.ylabel("F1 Score on Test Set")
+)
