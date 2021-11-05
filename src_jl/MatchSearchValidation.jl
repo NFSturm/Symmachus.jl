fefb160828614cd8088720b24e3d8feef7002fb0
NFSturm <niclas.sturm@live.de>
2021-11-05 16:46:18 +0000

    Moved kNN algorithm to separate file

--- src_jl/MatchSearchValidation.jl
+++ src_jl/MatchSearchValidation.jl
@@ -12,30 +12,8 @@ using Revise
 include("EncodingUtils.jl")
 .using EncodingUtils
 
-
-@doc """
-    compute_nearest_neighbours(comparison_point::Vector{Float32}, data::Vector{Vector{Float32}}, num_neighbors::Int64)::Tuple{Int64, Float32}
-
-Computes the `num_neighbors` from `data`. The data is supposed to be in the \n
-`encoding_column`. Returns the indices and distances.
-"""
-function compute_nearest_neighbours(comparison_point::Vector{Float32}, data::Vector{Vector{Float32}}, num_neighbors::Int64)::Tuple{Vector{Int64}, Vector{Float32}}
-
-    # Creating data matrices
-    data_matrix = hcat(data...)
-
-    # Instantiating the splitting tree
-    balltree = BallTree(data_matrix, Euclidean(); reorder = false)
-
-    # Computing indices and distances
-    # Catching the case when there are more requested neighbors than data points
-    idxs, dists = knn(balltree, comparison_point, min(num_neighbors, size(data_matrix)[2]), true)
-
-    sims = 1 .- cosine_dist.(Ref(comparison_point), data[idxs])
-
-    idxs, sims
-end
-
+include("kNN.jl")
+.using kNN
 
 @doc """
    similarity_search(name::String, encoding_model::Tuple{Symbol, Symbol}, encoded_speech_acts::DataFrame, encoded_activities::DataFrame)::DataFrame
