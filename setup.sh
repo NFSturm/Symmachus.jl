b03ead7abb017a47d24f41a404c3bbb756ba06ce
NFSturm <niclas.sturm@live.de>
2021-12-01 22:10:52 +0000

    Small change to initialization

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Setting up the project
 sudo dnf install julia git neovim unzip zip
 
-git clone https://github.com/NFSturm/Symmachus.jl.git
+#git clone https://github.com/NFSturm/Symmachus.jl.git
 cd Symmachus.jl
 julia setup_script.jl # Instantiating packages
 
