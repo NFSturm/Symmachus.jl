608ee352cc9d34a406ccac3f416931919d621eae
NFSturm <niclas.sturm@live.de>
2021-12-02 00:29:00 +0000

    Setup.sh makes sense now!

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Setting up the project
-sudo dnf install julia git neovim unzip zip
+sudo dnf install julia neovim unzip zip
 
-#git clone https://github.com/NFSturm/Symmachus.jl.git
 cd Symmachus.jl
 julia setup_script.jl # Instantiating packages
 
