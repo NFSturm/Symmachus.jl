b173e7e48422151e9a358de9f53b39aa833e30d8
NFSturm <niclas.sturm@live.de>
2021-12-10 12:25:36 +0000

    Commented in row numbers from sample

--- analyses/result_transparency/labelling_process.jl
+++ analyses/result_transparency/labelling_process.jl
@@ -7,6 +7,7 @@ using Serialization
 #******* DESERIALIZING LABELLING PROCESS RESULTS ********
 labelling_process_results = deserialize("./data/labels/labelled_data_final_21102021.jls")
 
+#[291966, 282405. 459641, 147016, 447045, 462333, 783968, 627807, 2560, 274177]
 row_samples = sample(1:nrow(labelling_process_results), 10)
 serialize(joinpath(@__DIR__, "row_samples.jls"), row_samples)
 
